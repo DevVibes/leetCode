e443490632763a00d106830f2cf3d6ad17567c85
Omkar Gawas <150122348+DevVibes@users.noreply.github.com>
2026-08-09 19:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (41.88%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         map<int,int> mp;
+    
+    for(int i=0;i<nums.size();i++)
+    {
+        mp[nums[i]]+=1;
+    }
+    
+    int maxcount=0;
+    int result=-1;
+    
+    for(auto &m:mp)
+    {
+        if(m.second > maxcount)
+            {
+                maxcount = m.second;
+                result = m.first;
+            }
+            
+    }
+    
+    return result;
+    }
+};

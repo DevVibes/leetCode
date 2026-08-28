aeee3cef9aab3aab554dcf7453146e03c5e8953b
Omkar Gawas <150122348+DevVibes@users.noreply.github.com>
2026-08-28 21:03:21 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.75%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
+    int singleNumber(const vector<int>& nums) {
        int e=nums[0];
        for(int i=1;i<nums.size();i++)
        {
@@ -10,29 +10,3 @@ public:
     }
 };
 
-
-const static auto io_speed_up = []() {
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    FILE* fptr = fopen("user.out", "w");
-    int x, out = 0;
-    while (!cin.eof()) {
-        if (cin.peek() == '[')
-            cin.ignore();
-        else
-            break;
-        out = 0;
-        while (cin.peek() != ']') {
-            cin >> x;
-            if (cin.peek() == ',')
-                cin.ignore();
-            out ^= x;
-        }
-        fprintf(fptr, "%d\n", out);
-        cin.ignore(1024, '\n');
-    }
-    fclose(fptr);
-    exit(0);
-    return 0;
-}();

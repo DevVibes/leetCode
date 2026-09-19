ff2a21d58c7cc005ae8adfface7ab04464b60eda
Omkar Gawas <150122348+DevVibes@users.noreply.github.com>
2026-09-19 22:00:41 +0530

    Time: 56 ms (13.55%) | Memory: 75.7 MB (31.89%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -8,25 +8,67 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ListNode* findmid(ListNode* head)
+{
+    if(head == nullptr || head->next == nullptr)
+            return head;
+    ListNode* slow = head;
+    ListNode* fast = head->next;
+    while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+    return slow;
+}
+
+ListNode* merge(ListNode* left, ListNode* right)
+{
+    ListNode* dummy = new ListNode(-1);
+    ListNode* temp = dummy;
+
+    while(left && right)
+    {
+        if(left->val < right->val)
+        {
+            temp->next = left;
+            temp = left;
+            left = left->next;
+        }
+        else
+        {
+            temp->next = right;
+            temp = right;
+            right=right->next;
+        }
+    }
+    if(left) temp->next =left;
+    else temp->next = right;
+
+    return dummy->next;
+
+}
+
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        vector<int> list;
-        ListNode* temp = head;
-        while(temp)
-        {
-            list.push_back(temp->val);
-            temp = temp->next;
-        }
-        sort(list.begin(), list.end());
-        temp = head;
-        for(int i=0;i<list.size();i++)
-        {
-            temp->val = list[i];
-            temp = temp->next;
-        }
+       
+        if(head == nullptr || head->next == nullptr)
+            return head;
 
-        return head;
+        ListNode* left = head;
+        ListNode* middle = findmid(head);
+        ListNode* right = middle->next;
+
+        middle->next = nullptr;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return merge(left,right);
+
     }
     
 };
